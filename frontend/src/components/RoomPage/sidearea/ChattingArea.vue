9658ce862adaf3221c2a1c01b4ed4ffb08abad30
bizyoung93 <bizyoung93@gmail.com>
2022-08-11 08:23:36 +0900

    FEAT: vuex store을 이용한 채팅 로직 구현 중입니다

--- frontend/src/components/RoomPage/sidearea/ChattingArea.vue
+++ frontend/src/components/RoomPage/sidearea/ChattingArea.vue
@@ -11,6 +11,14 @@
         <div class="message-send-content">레이아웃은 다 짜놨음</div>
       </div>
     </div>
+    <!-- 채팅 로그는 for 돌려 결정 -->
+    <div v-for="(chat, idx) in chattingLog" :key="idx" class="message">
+      <div class="message-user-info">
+        <div class="message-send-user">아무무</div>
+        <div class="message-send-time">오후 08:30</div>
+      </div>
+      <div class="message-send-content">레이아웃은 다 짜놨음</div>
+    </div>
     <!-- 받는 사람 정하는 부분 -->
     <div class="recipient-area">
       <div>받는 사람:</div>
@@ -39,9 +47,9 @@
   </div>
 </template>
 <script setup>
-const sendMessage = function () {
-  console.log('메시지 송출!')
-}
+import { watchEffect } from 'vue'
+import { useStore } from 'vuex'
+
 /* eslint-disable */
 const tempMessage = {
   userNickname: '김부겸',
@@ -54,10 +62,21 @@ let exName = ''
 
 let exTime = ''
 
+const store = useStore()
+
+const chattingLog = ref([])
+
+// 채팅 컴포넌트를 껐다가 켰을 때도 채팅이 유지되어야 함으로, 스토어에 저장해주기로 했습니다.
+watchEffect(() => {
+  if (store.getters.getChattings.length()) {
+    chattingLogs.value = store.getters.getChattings
+  }
+})
+
 // add Message하는 함수인데, 아직 통째로 돌릴지, 이렇게 할지 잘 모르겠어서 일단 만들어 놓는다.
 // eslint-disable-next-line
 const addMessage = function (message) {
-  console.log('메시지 받음!')
+  console.log('메시지 감지!!')
   const userName = message.userNickname
   const nowTime = new Date().toLocaleTimeString().slice(0, -3)
   const messageBox = document.createElement('div')
